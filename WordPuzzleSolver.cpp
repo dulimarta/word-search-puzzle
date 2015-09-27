70d04fa7ea59935df5d4d060f099a115a8d10ddf
Hans Dulimarta <hans.dulimarta@gmail.com>
2015-09-27 08:49:30 -0400

    Remove solve_own() (the first version of triple).

--- WordPuzzleSolver.cpp
+++ WordPuzzleSolver.cpp
@@ -34,8 +34,7 @@ WordPuzzleSolver::WordPuzzleSolver(ifstream &fin) {
 void WordPuzzleSolver::solve() {
 //    solve_with_slow_triple();
 //    solve_with_fast_triple();
-//    solve_with_quad();
-    solve_own();
+    solve_with_quad();
 }
 
 /** TRIPLE */
@@ -150,7 +149,7 @@ vector<tuple<int,int,string>>& sol) const
             return true;
         }
 
-//        /* Use a traditional for-loop the above code is */
+//        /* Using a traditional for-loop the above code is */
 //        for (int i = 0; i < all_words.size(); ++i) {
 //            if (word == all_words[i]) {
 //                sol[i]  = make_tuple(start_row, start_col, "DOWN");
@@ -181,11 +180,13 @@ void WordPuzzleSolver::solve_with_quad() const {
         if (minLen > w.size()) minLen = w.size();
         if (maxLen < w.size()) maxLen = w.size();
     }
-    /* allocate another array to hold the solution */
+    /* allocate another array to hold the solution,
+     * the first two integers in the tuple are the row,col positions
+     * the third is ACROSS/DOWN */
     vector<tuple<int,int,string>> solution (all_words.size());
     for (int k = 0; k < grid.size(); k++) {
         for (int m = 0; m < grid[0].size(); m++) {
-            /* we have to check BOTH direction */
+            /* we have to check BOTH directions */
             check_quad_across (k, m, minLen, maxLen, solution);
             check_quad_down (k, m, minLen, maxLen, solution);
         }
@@ -196,50 +197,5 @@ void WordPuzzleSolver::solve_with_quad() const {
             << get<0>(my_tuple) << " "
             << get<1>(my_tuple) << " "
             << get<2>(my_tuple) << endl;
-    }
-}
-
-void WordPuzzleSolver::solve_own() {
-    std::map<string,tuple<int,int,string>> result;
-    for (auto& x : words_by_length) {
-        const int N = x.first;
-        auto& word_set = x.second;
-//        clog << word_set.size() << " words of length " << N << endl;
-        for (int k = 0; k < grid.size(); k++) {
-            for (int m = 0; m < grid[k].size() - N + 1; m++) {
-                string word;
-                for (int c = 0; c < N; c++) {
-                    word += grid[k][m+c];
-                }
-                if (word_set.find(word) != word_set.end()) {
-//                    clog << word << " " << k << " " << m << " ACROSS" << endl;
-                    word_set.erase(word);
-                    result[word] = make_tuple(k, m, "ACROSS");
-                }
-            }
-        }
-        for (int k = 0; k < grid.size() - N + 1; k++) {
-            for (int m = 0; m < grid[k].size(); m++) {
-                string word;
-                for (int c = 0; c < N; c++) {
-                    word += grid[k+c][m];
-                }
-                if (word_set.find(word) != word_set.end()) {
-//                    clog << word << " " << k << " " << m << " DOWN" << endl;
-                    result[word] = make_tuple(k, m, "DOWN");
-                    word_set.erase(word);
-                }
-            }
-        }
-    }
-
-    for (string& s : all_words) {
-        auto x = result.find(s);
-        if (x != result.end()) {
-            tuple<int, int, string> &info = x->second;
-            cout << s << " " << get<0>(info) << " " << get<1>(info) << " " << get<2>(info) << endl;
-        }
-        else
-            cerr << "Word " << s << " is not in the grid?" << endl;
     }
 }
